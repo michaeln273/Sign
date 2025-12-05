172902333dc53203e17f28cac8c0f1c3e2c7d566
Michael Nikoloudakis <michaeln273@nycstudents.net>
2025-12-05 13:02:54 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+        Sign sign1 = new Sign("ABC222DE", 3);
+        System.out.println(sign1.numberOfLines());
+        System.out.println(sign1.getLines());
+        System.out.println(sign1.getLines());
+
+        Sign sign2 = new Sign("ABCD", 10);
+        System.out.println(sign2.numberOfLines());
+        System.out.println(sign2.getLines());
+
+        Sign sign3 = new Sign("ABCDEF", 6);
+        System.out.println(sign3.numberOfLines());
+        System.out.println(sign3.getLines());
+
+        Sign sign4 = new Sign("", 4);
+        System.out.println(sign4.numberOfLines());
+        System.out.println(sign4.getLines());
+
+        Sign sign5 = new Sign("AB_CD_EF", 2);
+        System.out.println(sign5.numberOfLines());
+        System.out.println(sign5.getLines());
+
+//        Sign sign6 = new Sign("Everything on sale, please come in", 15);
+//        System.out.println(sign6.numberOfLines());
+//        System.out.println(sign6.getLines());
+    }
+}
---
+++ src/Sign.java
@@ -0,0 +1,31 @@
+public class Sign {
+    private String message;
+    private int width;
+    public Sign(String message, int width) {
+        this.message = message;
+        this.width = width;
+    }
+
+    public int numberOfLines() {
+        int len = message.length();
+        if (len == 0) return 0;
+        if (len < width) return 1;
+        return (int)Math.round(len / (double)width);
+    }
+
+    public String getLines() {
+        String lines = "";
+        int last = 0;
+        int numLines = numberOfLines();
+
+        if (numLines == 0) return null;
+
+        for (int i = 0; i < numLines - 1; i++) {
+            lines += message.substring(last, last + width);
+            last += width;
+            lines += ';';
+        }
+        lines += message.substring(last);
+        return lines;
+    }
+}
